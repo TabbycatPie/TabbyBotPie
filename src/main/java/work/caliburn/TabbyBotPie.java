f39aefb0310c688bea6a586136ccafead3269817
TabbyCatPie <78999461+TabbycatPie@users.noreply.github.com>
2022-12-14 15:31:31 +0800

    update

--- src/main/java/work/caliburn/TabbyBotPie.java
+++ src/main/java/work/caliburn/TabbyBotPie.java
@@ -3,9 +3,14 @@ package work.caliburn;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
 import net.mamoe.mirai.event.GlobalEventChannel;
+import net.mamoe.mirai.utils.MiraiLogger;
 
 public final class TabbyBotPie extends JavaPlugin {
     public static final TabbyBotPie INSTANCE = new TabbyBotPie();
+
+
+    private boolean isEnabled = false;
+
 
     private TabbyBotPie() {
         super(new JvmPluginDescriptionBuilder("work.caliburn.tabbybotpie", "0.1.0")
@@ -16,8 +21,10 @@ public final class TabbyBotPie extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        GlobalEventChannel.INSTANCE.registerListenerHost(new Test());
-        GlobalEventChannel.INSTANCE.registerListenerHost(new GroupMsgEventListener());
         getLogger().info("Plugin loaded!");
+        GlobalEventChannel.INSTANCE.registerListenerHost(new Test());
+        getLogger().info("Test() Listener registered.");
+        GlobalEventChannel.INSTANCE.registerListenerHost(new GroupMsgEventListener());
+        getLogger().info("GroupMsgEventListener() Listener registered.");
     }
 }
